d44aeee5464a3c65ffcd7f52a9c841107ecabe50
Arthur de Araujo Custódio <141514740+ArthurDevA@users.noreply.github.com>
2024-09-10 14:19:11 -0300

    mudança no filtro (1)

--- BolsaInforma/BolsaInforma/Componentes/FilterButtonView.swift
+++ BolsaInforma/BolsaInforma/Componentes/FilterButtonView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct FilterButtonView: View {
     
     @ObservedObject var filterButton: FilterButton
-    
+//teste de merge Matheus
     func getRectColor(isSelected: Bool) -> Color {
         if (isSelected) {
             Color.tinta
@@ -42,14 +42,15 @@ struct FilterButtonView: View {
                 RoundedRectangle(cornerRadius: 20)
                     .fill(rectColor)
                     .strokeBorder(Color(.tinta))
-                    .frame(width: 77)
+                    .frame(/*width: 77, */height: 27)
                 
                     .aspectRatio(contentMode: .fill)
                 Text(filterButton.text)
                     .font(.system(size: 16))
                     .fontWeight(.semibold)
                     .foregroundStyle(textColor)
-                    .frame(width: 60, height: 21)
+                    .frame(/*width: 60, */height: 27)
+                    .padding()
                 
 //            }
         }
